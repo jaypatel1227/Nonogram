32d002e64c22c58df9fda14f872608bd622988be
jaypatel1227 <jaypatel122700@gmail.com>
2021-05-18 15:00:45 -0400

    first commit

---
+++ nonogram.py
@@ -0,0 +1,16 @@
+''' 
+    This is a library that allows you to generate and play a type of puzzle called a ``nonogram''. 
+    A nonogram is presented as a grid where each square is either filled in or not. The object is 
+    to figure out exactly which of the squares are supposed to be filled in. You are given two 
+    lists as clues. The first list tells of you the the structure of the filled in spaces in each 
+    of the rows. For example,
+            the row: xxx--x---x (where x is filled in and - is open) is given the clue (3,1,1).
+    Similarly, the second list tells you the same structuce of the columns. Now, you may even get 
+    the list in a state where some of the squares are alreadly filled in to makle this easier or 
+    remove ambiguity in solutions. 
+'''
+class Nonogram():
+    def __init__(self):
+        pass
+
+
